24a17fc687f737c67031257ef5b33bde0df4fac5
ookura-mf <ookura.keisuke@moneyforward.co.jp>
2021-12-28 17:34:13 +0900

    Change jdk version from 17 to 11.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.okeicalm"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -34,7 +34,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = "11"
 	}
 }
 
