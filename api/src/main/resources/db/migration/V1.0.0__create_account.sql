698e2ee11f2543aa6a44e548e2ffe479a97e5494
ookura-mf <ookura.keisuke@moneyforward.co.jp>
2021-12-31 14:20:57 +0900

    Fix DDL.

--- api/src/main/resources/db/migration/V1.0.0__create_account.sql
+++ api/src/main/resources/db/migration/V1.0.0__create_account.sql
@@ -3,6 +3,6 @@ use simple_journal_entry_db;
 create table accounts (
                        id int not null primary key auto_increment,
                        code varchar(50) not null unique,
-                       name varchar(50),
+                       name varchar(50) not null,
                        element_type int not null
 );
