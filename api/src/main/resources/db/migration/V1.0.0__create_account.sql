d1eeb01b8d5f9ce366b34ddd9c657f2fc8fa145f
ookura-mf <ookura.keisuke@moneyforward.co.jp>
2022-01-03 10:47:24 +0900

    Fix column type of id.

--- api/src/main/resources/db/migration/V1.0.0__create_account.sql
+++ api/src/main/resources/db/migration/V1.0.0__create_account.sql
@@ -1,7 +1,7 @@
 use simple_journal_entry_db;
 
 create table accounts (
-                       id int not null primary key auto_increment,
+                       id bigint not null primary key auto_increment,
                        code varchar(50) not null unique,
                        name varchar(50) not null,
                        element_type int not null
