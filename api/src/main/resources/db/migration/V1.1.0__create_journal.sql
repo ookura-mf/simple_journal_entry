7df1faa24864d480ba69b1a3ed9c69b82ba4fd56
ookura-mf <ookura.keisuke@moneyforward.co.jp>
2022-01-13 01:55:48 +0900

    Fix columns.

--- api/src/main/resources/db/migration/V1.1.0__create_journal.sql
+++ api/src/main/resources/db/migration/V1.1.0__create_journal.sql
@@ -3,5 +3,5 @@ use simple_journal_entry_db;
 create table journals
 (
     id          bigint not null primary key auto_increment,
-    incurred_on DATE   not null unique
+    incurred_on DATE   not null
 );
