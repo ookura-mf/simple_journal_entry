3717b295345059a3321e1014d303e6d765c5c781
okeicalm <okeicalm@gmail.com>
2022-06-11 23:51:31 +0900

    Enable custom scalar.

--- api/src/main/kotlin/com/okeicalm/simplejournalentry/SimpleJournalEntryApplication.kt
+++ api/src/main/kotlin/com/okeicalm/simplejournalentry/SimpleJournalEntryApplication.kt
@@ -1,12 +1,17 @@
 package com.okeicalm.simpleJournalEntry
 
+import com.okeicalm.simpleJournalEntry.handler.scalar.CustomSchemaGeneratorHooks
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
 @EnableTransactionManagement
-class SimpleJournalEntryApplication
+class SimpleJournalEntryApplication {
+    @Bean
+    fun customGraphQLSchemaGeneratorHooks() = CustomSchemaGeneratorHooks()
+}
 
 fun main(args: Array<String>) {
     runApplication<SimpleJournalEntryApplication>(*args)
