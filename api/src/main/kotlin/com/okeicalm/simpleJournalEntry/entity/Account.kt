e5209f3800e8561a934a017002ddabcbc044c79f
okeicalm <okeicalm@gmail.com>
2022-06-12 22:30:33 +0900

    Change type of elementType to enum

--- api/src/main/kotlin/com/okeicalm/simpleJournalEntry/entity/Account.kt
+++ api/src/main/kotlin/com/okeicalm/simpleJournalEntry/entity/Account.kt
@@ -1,8 +1,10 @@
 package com.okeicalm.simpleJournalEntry.entity
 
+import com.okeicalm.simpleJournalEntry.valueobject.AccountElementType
+
 data class Account(
     val id: Long = 0,
     val code: String,
     val name: String,
-    val elementType: Int,
+    val elementType: AccountElementType,
 )
