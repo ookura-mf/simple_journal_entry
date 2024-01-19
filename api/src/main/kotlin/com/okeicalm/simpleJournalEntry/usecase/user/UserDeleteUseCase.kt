4a8eeb6fde51e16f37fbfe8f76880ca7a771bb16
nohara.koji <nohara.koji@moneyforward.co.jp>
2024-01-19 16:48:45 +0900

    fix: apply for the case where target user doesn't exit

--- api/src/main/kotlin/com/okeicalm/simpleJournalEntry/usecase/user/UserDeleteUseCase.kt
+++ api/src/main/kotlin/com/okeicalm/simpleJournalEntry/usecase/user/UserDeleteUseCase.kt
@@ -1,20 +1,21 @@
 package com.okeicalm.simpleJournalEntry.usecase.user
 
+import com.okeicalm.simpleJournalEntry.entity.User
 import com.okeicalm.simpleJournalEntry.repository.UserRepository
 import org.springframework.stereotype.Service
 
 data class UserDeleteUseCaseInput(val id: Long)
-data class UserDeleteUseCaseOutput(val id: Long)
+data class UserDeleteUseCaseOutput(val user: User?)
 
 interface UserDeleteUseCase {
     fun call(input: UserDeleteUseCaseInput): UserDeleteUseCaseOutput
 }
 
 @Service
-class UserDeleteUseCaseImpl(private val repository: UserRepository): UserDeleteUseCase {
+class UserDeleteUseCaseImpl(private val userRepository: UserRepository): UserDeleteUseCase {
     override fun call(input: UserDeleteUseCaseInput): UserDeleteUseCaseOutput {
-        val id = input.id
-        return UserDeleteUseCaseOutput(repository.delete(id))
+        val deletedUser = userRepository.findById(input.id) ?: return UserDeleteUseCaseOutput(null)
+        userRepository.delete(input.id)
+        return UserDeleteUseCaseOutput(deletedUser)
     }
-
 }
